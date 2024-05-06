30bf3f0a442d4b9788b96849cc5a492a4d86add0
NetAcad DEVASC <changeme@example.com>
2024-05-06 01:25:44 +0000

    Mi primer commit :)

---
+++ graphhopper/graphhopper_parse-json_1.py
@@ -0,0 +1,15 @@
+import requests
+import urllib.parse
+
+geocode_url = "https://graphhopper.com/api/1/geocode?"
+route_url = "https://graphhopper.com/api/1/route?"
+loc1 = "Rome, Italy"
+loc2 = "Baltimore, Maryland"
+key = "f180ebac-3631-45d1-bf58-2ee219f73784"      # Replace with your Graphhopper API key
+
+url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+
+replydata = requests.get(url)
+json_data = replydata.json()
+json_status = replydata.status_code
+print(json_data)
